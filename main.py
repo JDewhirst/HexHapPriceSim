959c94f609ead9e395693d40fc2e39ed1f9e0810
JDewhirst <43847044+JDewhirst@users.noreply.github.com>
2020-08-25 17:37:35 +0100

    Update main.py
    Added try/except. Checks if an entry exists for distances[end][begin] i.e. (checks if we have computed the reverse of this journey). If there is, takes its value and applies it for the distances[begin][end], and takes the existing paths[end][begin] entry and sets it as the paths[begin][end] entry

--- main.py
+++ main.py
@@ -11,24 +11,28 @@ import read_hxm
 import local_price
 
 graph = read_hxm.makeGraph("testmap.hxm")#("testmap2.hxm")
-markets = ['00.01', '17.03', '43.11', '38.23', '05.13',
-        '01.19', '13.19', '25.19'
+markets = ['00.01', '05.05', '08.08'
     ]
 paths = []
 distances = []
 
 for begin in range(len(markets)):
-    #print('tic')
     placeholder_distance = []
     placeholder_paths = []
     for end in range(len(markets)):
-        #print('toc')
-        if begin == end:
-            placeholder_distance.append(1)
-            placeholder_paths.append(markets[begin])
-        else:
-            a, b = pathfinding.Dijkstra(graph,markets[begin],markets[end]) 
-            placeholder_distance.append(a), placeholder_paths.append(b)
+        try:
+            #if distance end->begin already calced, just sub it in
+            #same for path
+            if distances[end][begin]:
+                placeholder_distance.append(distances[end][begin])
+                placeholder_paths.append(reverse(paths[end][being]))
+        except:
+             if begin == end:
+                 placeholder_distance.append(1)
+                 placeholder_paths.append(markets[begin])
+             else:
+                 a, b = pathfinding.Dijkstra(graph,markets[begin],markets[end]) 
+                 placeholder_distance.append(a), placeholder_paths.append(b)
             
     distances.append(placeholder_distance)
     paths.append(placeholder_paths)
